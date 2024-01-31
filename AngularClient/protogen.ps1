023d7398a6d9d6ddcacb6e4e9c9089b047e10d41
sidanas22 <50417848+sidanas22@users.noreply.github.com>
2024-01-31 18:11:52 +0500

    fix: protogen script fix

--- AngularClient/protogen.ps1
+++ AngularClient/protogen.ps1
@@ -1,7 +1,15 @@
-$PROTOC_GEN_TS_PATH = "$($PWD)\node_modules\.bin\protoc-gen-ts.cmd"
+# $PROTOC_GEN_TS_PATH = "$($PWD)\node_modules\.bin\protoc-gen-ts.cmd"
 
+# $PROTOC_OUT_DIR = "$($PWD)\src\app\core"
+
+# $PROTOC_FILE_NAMES = $args
+
+# protoc --plugin="protoc-gen-ts=$PROTOC_GEN_TS_PATH" --js_out="import_style=commonjs,binary:$PROTOC_OUT_DIR" --ts_out="service=grpc-web:$PROTOC_OUT_DIR" grpc-models/$PROTOC_FILE_NAMES
+
+
+$PROTOC_GEN_TS_PATH = "$($PWD)\node_modules\.bin\protoc-gen-ts.cmd"
 $PROTOC_OUT_DIR = "$($PWD)\src\app\core"
 
-$PROTOC_FILE_NAMES = $args
-
-protoc --plugin="protoc-gen-ts=$PROTOC_GEN_TS_PATH" --js_out="import_style=commonjs,binary:$PROTOC_OUT_DIR" --ts_out="service=grpc-web:$PROTOC_OUT_DIR" grpc-models/$PROTOC_FILE_NAMES
+foreach ($file in $args) {
+    protoc --plugin="protoc-gen-ts=$PROTOC_GEN_TS_PATH" --js_out="import_style=commonjs,binary:$PROTOC_OUT_DIR" --ts_out="service=grpc-web:$PROTOC_OUT_DIR" "grpc-models/$file"
+}
